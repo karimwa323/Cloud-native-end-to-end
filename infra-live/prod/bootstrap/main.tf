e26b921f2a76a9eab2190bffebb521e0b7438a9b
karimwa323 <karimwa323@gmail.com>
2026-03-18 05:04:50 +0200

    Adding security stepd

--- infra-live/prod/bootstrap/main.tf
+++
@@ -1,8 +0,0 @@
-module "backend" {
-  source = "../../modules/backend"
-
-  bucket_name         = "karim-eks-terraform-state-prod"
-  dynamodb_table_name = "terraform-state-lock-prod"
-  environment         = "prod"
-}
-
